42cd21df2a089308f31d573ce73254e33b70da4e
Samuel Gómez <profesor_s@outlook.com>
2019-02-11 19:03:40 +0100

    Primos

--- 02primos/primos.cpp
+++ 02primos/primos.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 
 bool esPrimo(int);
 int cotaSuperior=100;
-
+ 
 
 int main(void){
+	ofstream fPrimes;
+	fPrimes.open("primos.txt");
 	cout<<"Cota superior: ";
 	cin>>cotaSuperior;
 
+	int percent = 0;
 	for (int n=1; n<=cotaSuperior; n++){
 		if (esPrimo(n)){
-			cout<<n<<endl;
+			//cout<<setw(2)<<(int)(n*100.0/cotaSuperior)<<"% "<<n<<endl;
+			int percentTmp = (int)(n*100.0/cotaSuperior);
+			if (percentTmp!=percent){
+				percent = percentTmp;
+				cout<<setw(2)<<percent<<"%"<<endl;
+			}
+			fPrimes<<n<<endl;
 		}
 	}
+	fPrimes.close();
+	return 0;
 }
 
 bool esPrimo(int n){
