cb134b8ebffa851e3f9d2ab5d64e13e388eff104
Samuel Gómez <profesor_s@outlook.com>
2019-02-21 20:55:11 +0100

    Lectura fácil con ifstream

--- 04cargas/src/main.cc
+++ 04cargas/src/main.cc
@@ -5,34 +5,20 @@
 #include <math.h>
 #include <vector>
 
-#include <boost/algorithm/string.hpp>
-
 using namespace std;
 using namespace CampoElectrico;
 
-const int NCARGAS = 3;
-
-
 int main(){
-	//std::unique_ptr<Carga> q1(new Carga(0,4,1e-9));
-
-	ifstream fCargas;
-	fCargas.open("cargas.txt", ifstream::in);
-
-	int i = 0;
-	string sLine;
+	ifstream fCargas("cargas.txt");
 	CampoElectrico::Vector2d E={0, 0};
-	while(getline(fCargas, sLine)){
-		vector<string> arrLine;
-	    split(arrLine, sLine, boost::is_any_of(" "));	// [0] carga.x	[1]	carga.y	[2] carga.q
-		CampoElectrico::Vector2d E_i = getE(0, 0, stof(arrLine[0]), stof(arrLine[1]), stol(arrLine[2]));
+	float x,y;
+	double q;
+	while(fCargas>>x>>y>>q){
+		CampoElectrico::Vector2d E_i = getE(0, 0, x, y, q);
 		E.x += E_i.x;
 		E.y += E_i.y;
 	}
-
 	fCargas.close();
-
 	cout<<E.x<<"i "<<E.y<<"j "<<endl;
-
 	return 0;
 }
